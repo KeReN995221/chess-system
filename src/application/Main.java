6772b82b3ca93e876ef1d4327e0eb054970d38f3
Keren Apuque <cardosokerenapuque@gmail.com>
2024-03-31 17:11:00 -0300

    started to implement Board and Piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,14 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        Board board = new Board(8,8);
+         
+
     }
 }
