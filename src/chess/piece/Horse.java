120ed1fc71007912ef6afb2feb7d9fbbdc7d60ea
Keren Apuque <cardosokerenapuque@gmail.com>
2024-03-31 22:29:03 -0300

    Placing pieces on the board

---
+++ src/chess/piece/Horse.java
@@ -0,0 +1,17 @@
+package chess.piece;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Horse extends ChessPiece {
+    public Horse(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString(){
+        return "H";
+    }
+
+}
